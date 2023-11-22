db67a825f542bf913dce6a4fa9be9eccb9cc209c
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-11-22 14:01:39 -0600

    Fix navbar in large screens

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -7,21 +7,21 @@ export default function Menu() {
     const {mobileMenuOn} = useContext(MobileMenuContext)
 
     return(
-        <div className={`fixed flex-col w-[86%] left-[7%] top-[100px] bg-dark-secondary rounded-lg p-8 font-bold z-10 lg:flex-row ${mobileMenuOn ? `flex` : `hidden`}`}>
+        <div className={`fixed flex-col w-[86%] left-[7%] top-[100px] bg-dark-secondary rounded-lg p-8 font-bold z-10 lg:flex-row lg:flex lg:p-0 lg:bg-transparent lg:static ${mobileMenuOn ? `flex` : `hidden`}`}>
             <Button
                 color="txt"
                 bg="secondary"
                 className="order-2 h-btn lg:border-none lg:w-[120px]"
             >Spanish</Button>
-            <nav className="order-1 lg:order-2">
-                <ul className="flex flex-col items-center space-y-1 lg:rounded-lg lg:overflow-hidden lg:items-center lg:dark:bg-dark-secondary">
+            <nav className="order-1 lg:order-2 lg:mx-auto">
+                <ul className="flex flex-col items-center space-y-1 lg:flex-row lg:items-center lg:space-y-0 lg:rounded-lg lg:dark:bg-dark-secondary">
                     <MenuNavItem>Home</MenuNavItem>
                     <MenuNavItem>About</MenuNavItem>
                     <MenuNavItem>Work</MenuNavItem>
                     <MenuNavItem>Contact</MenuNavItem>
                 </ul>
             </nav>
-            <div className="h-[1px] my-6 w-full bg-dark-primary order-1 opacity-50"></div>
+            <div className="h-[1px] my-6 w-full bg-dark-primary order-1 opacity-50 lg:hidden"></div>
         </div>
     )
 }
