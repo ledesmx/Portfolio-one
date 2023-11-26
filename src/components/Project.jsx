837278fc707651e8903d3c22152dabb9806d7689
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-11-26 16:55:23 -0600

    Add the Project layout for small screens

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -30,23 +30,29 @@ export default function Project(props) {
     }
 
     return(
-        <div className={`border-2 rounded-3xl py-[60px] px-[7%] ${borderColor()}`}>
-            <span>{props.id}</span>
+        <div className={`border-2 rounded-3xl py-[50px] px-[7%] relative ${borderColor()}`}>
+            <span className="font-jetbrains text-2xl absolute left-[4%] top-[15px]">{props.id}</span>
             <div>
-                <h3 className={`${textColor()}`}>{props.title}</h3>
-                <p>{props.description}</p>
-                <ul className="flex space-x-2">
+                <h3 className={`font-koulen text-5xl mb-5 ${textColor()}`}>{props.title}</h3>
+                <p className="font-encode leading-8 mb-4">{props.description}</p>
+                <ul className="flex flex-wrap justify-evenly mb-8">
                     {props.skills.map(item => 
                     <li
-                        key={item}
-                    ><span className={`${textColor()}`}>~</span> {item}</li>)}
+                    className="font-jetbrains flex flex-nowrap space-x-1"
+                    key={item}
+                    ><span className={`${textColor()}`}>~ </span><span>{item}</span></li>)}
                 </ul>
             </div>
             <div>
-                <img src={props.img} alt="project img" />
+                <img 
+                className="rounded-xl mb-10"
+                src={props.img} alt="project img" />
                 <div>
-                    <Button color={props.color}>Code</Button>
+                    <Button
+                    className="w-full h-btn mb-4"
+                    color={props.color}>Code</Button>
                     <Button 
+                    className="w-full h-btn"
                     color="secondary"
                     bg={props.color}>Visit site</Button>
                 </div>
