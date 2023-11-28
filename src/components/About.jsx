99b2e1717cb98590dbf45a3bfbf05144d255c148
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-11-28 17:43:29 -0600

    Improve space between components in small screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import personalPhoto from "../../public/personal-photo.png"
 export default function About() {
     return(
         <section id="about"
-        className="w-[86%] max-w-[1100.8px] mx-auto grid gap-y-10 py-40 md:gap-y-5">
+        className="w-[86%] max-w-[1100.8px] mx-auto grid gap-y-10 py-20 md:gap-y-5 lg:py-40">
             <div className="grid grid-cols-1 gap-y-10 md:grid-cols-3 md:gap-x-5 lg:grid-cols-4 lg:grid-rows-1">
                 <h2 className="font-jetbrains uppercase text-3xl text-center text-light-txt dark:text-dark-txt md:col-span-3 lg:col-span-1 lg:text-left">Who I am?</h2>
                 <img 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import { projects } from "../projects-info"
 export default function Work() {
     return(
         <div id="work"
-        className="bg-light-secondary dark:bg-dark-secondary rounded-5xl py-40 flex justify-center">
+        className="bg-light-secondary dark:bg-dark-secondary rounded-5xl py-20 flex justify-center lg:py-40">
             <section className="w-[86%] max-w-[1100.8px] mx-auto text-light-neutral dark:text-dark-neutral">
                 <h2 className="font-jetbrains uppercase text-3xl text-center mb-10 lg:text-left">My work</h2>
                 <div className="space-y-10">
