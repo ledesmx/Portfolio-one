7e14ae4b546f2bd922c4bfeff84fd14421ec1ac9
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-12-07 18:39:46 -0600

    Improve Contact text and now the anchor tags work

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,10 +10,10 @@ export default function Contact() {
             <div className="text-light-txt dark:text-dark-txt lg:col-span-3 lg:text-lg">
                 <div className="h-[2px] w-full bg-light-txt dark:bg-dark-txt my-7 mt-0 lg:mt-2 lg:my-[60px]"></div>
                 <h3 className="font-koulen text-4xl lg:text-5xl lg:mx-[80px]">I’m always up for a chat.</h3>
-                <p className="font-encode mt-6 leading-7 lg:mx-[80px]">Please reach out if you have any questions! I'm happy to jump on a video call. Send me an email at alberto.ledesma.dev@gmail.com</p>
+                <p className="font-encode mt-6 leading-7 lg:mx-[80px]">If you have any queries, feel free to contact me! I would be glad to schedule a video call with you. You can drop me an email at alberto.ledesma.dev@gmail.com.</p>
                 <div className="h-[2px] w-full bg-light-txt dark:bg-dark-txt my-7 lg:my-[60px]"></div>
                 <h3 className="font-koulen text-4xl lg:text-5xl lg:mx-[80px]">Frontend mentor community.</h3>
-                <p className="font-encode mt-6 leading-7 lg:mx-[80px]">You can find me in the Frontend Mentor community, where I've started helping others with their code.</p>
+                <p className="font-encode mt-6 leading-7 lg:mx-[80px]">I have initiated assisting others with their code in the Frontend Mentor community. You can spot me there.</p>
                 <div className="h-[2px] w-full bg-light-txt dark:bg-dark-txt my-7 lg:my-[60px]"></div>
             </div>
             <div className="bg-light-txt text-light-secondary dark:bg-dark-txt dark:text-dark-secondary px-5 py-8 rounded-2xl space-y-5 flex flex-col items-center lg:col-span-4 lg:mt-12 lg:flex-row lg:space-y-0 lg:py-10 lg:justify-evenly">
@@ -21,15 +21,17 @@ export default function Contact() {
                 <div className="flex space-x-8 text-4xl lg:text-5xl">
                     <a
                     className="hover:opacity-75 transition-opacity" 
-                    href=""
+                    href="https://github.com/AlbertoLed"
+                    target="_blank"
                     ><FaGithub /></a>
                     <a
                     className="hover:opacity-75 transition-opacity" 
-                    href=""
+                    href="https://www.frontendmentor.io/profile/AlbertoLed"
+                    target="_blank"
                     ><SiFrontendmentor /></a>
                     <a
                     className="hover:opacity-75 transition-opacity" 
-                    href=""
+                    href="mailto:alberto.ledesma.dev@gmail.com"
                     ><IoMdMail /></a>
                 </div>
             </div>
