d106a3e593f35d32028576f7be8042bc7645ab5c
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-12-10 17:37:05 -0600

    Add Header animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header(props) {
     return(
         <header id="home"
         className="min-h-[100vh] flex flex-col items-center justify-center text-light-primary dark:text-dark-primary">
-            <div className="mb-auto mt-[20px] flex w-[86%] max-w-[1100.8px]">
+            <div className="mb-auto mt-[20px] flex w-[86%] max-w-[1100.8px]"            >
                 <MenuContext>
                     <MenuButton />
                     <Menu />    
@@ -18,18 +18,63 @@ export default function Header(props) {
                 onClick={props.toggleDarkTheme}
                 >Theme</Button>
             </div>
-            <h1 className="w-[86%] max-w-[1100.8px] text-center font-koulen text-6xl sm:text-9.1xl lg:text-9xl lg:text-left xl:text-13xl">
-                <span className="flex flex-col lg:w-[875.483px] lg:mx-auto xl:w-auto xl:mx-0">
-                    <span>
-                    Hey, I’m <span className="font-fira font-light text-light-txt dark:text-dark-txt">{`=>`}</span>
+                <h1 className="text-center font-koulen text-6xl sm:text-9.1xl lg:text-9xl lg:text-left xl:text-13xl">
+                    <span className="flex">
+                        <span className="animate-grow-80 lg:animate-grow-100">H</span>
+                        <span className="animate-grow-70 lg:animate-grow-90">e</span>
+                        <span className="animate-grow-60 lg:animate-grow-80">y</span>
+                        <span className="animate-grow-50 lg:animate-grow-70 mr-5 lg:mr-7 xl:mr-12">,</span>
+                        <span className="animate-grow-40 lg:animate-grow-60">I</span>
+                        <span className="animate-grow-50 lg:animate-grow-50">’</span>
+                        <span className="animate-grow-60 lg:animate-grow-40 mr-5 lg:mr-7 xl:mr-12">m</span>
+                        <span className="animate-grow-70 lg:animate-grow-30 font-fira font-light text-light-txt dark:text-dark-txt">{`=>`}</span>
                     </span>
-                    <span>
-                    <span className="font-fira font-light text-light-txt dark:text-dark-txt">{`{`}</ span>Alberto Ledesma<span className="font-fira font-light text-light-txt dark:text-dark-txt">{`}`}</span>  
+                    <span className="flex flex-col lg:flex-row">
+                        <span className="flex justify-center">
+                            <span className="animate-grow-50 lg:animate-grow-70 font-fira font-light text-light-txt dark:text-dark-txt">{`{`}</ span>
+                            <span className="animate-grow-40 lg:animate-grow-60">A</span>
+                            <span className="animate-grow-30 lg:animate-grow-50">l</span>
+                            <span className="animate-grow-20 lg:animate-grow-40">b</span>
+                            <span className="animate-grow-30 lg:animate-grow-30">e</span>
+                            <span className="animate-grow-40 lg:animate-grow-20">r</span>
+                            <span className="animate-grow-50 lg:animate-grow-10">t</span>
+                            <span className="animate-grow-60 lg:animate-grow-0 lg:mr-7 xl:mr-12">o </span>
+                        </span>
+                        <span className="flex justify-center">
+                            <span className="animate-grow-30 lg:animate-grow-0">L</span>
+                            <span className="animate-grow-20 lg:animate-grow-10">e</span>
+                            <span className="animate-grow-10 lg:animate-grow-20">d</span>
+                            <span className="animate-grow-0 lg:animate-grow-30">e</span>
+                            <span className="animate-grow-0 lg:animate-grow-40">s</span>
+                            <span className="animate-grow-10 lg:animate-grow-50">m</span>
+                            <span className="animate-grow-20 lg:animate-grow-60">a</span>
+                            <span className="animate-grow-30 lg:animate-grow-70 font-fira font-light text-light-txt dark:text-dark-txt">{`}`}</span>  
+                        </span>
+                        
                     </span>
-                </span>
-                
-            </h1>
-            <p className="font-jetbrains text-2xl tracking-wide mt-20 mb-auto opacity-80 text-light-primary dark:text-dark-primary lg:text-4xl">Front-End Developer</p>
+                </h1>
+            <p className="font-jetbrains flex text-xl mt-20 mb-auto opacity-70 text-light-primary dark:text-dark-primary lg:text-2xl">
+                <span className="animate-type-0 mr-[12px] lg:mr-[16px]">/</span>
+                <span className="animate-type-10 mr-[.5px]">F</span>
+                <span className="animate-type-20 mr-[.5px]">r</span>
+                <span className="animate-type-30 mr-[.5px]">o</span>
+                <span className="animate-type-40 mr-[.5px]">n</span>
+                <span className="animate-type-50 mr-[.5px]">t</span>
+                <span className="animate-type-60 mr-[.5px]">-</span>
+                <span className="animate-type-70 mr-[.5px]">E</span>
+                <span className="animate-type-80 mr-[.5px]">n</span>
+                <span className="animate-type-90 mr-[12px] lg:mr-[16px]">d</span>
+                <span className="animate-type-100 mr-[.5px]">D</span>
+                <span className="animate-type-110 mr-[.5px]">e</span>
+                <span className="animate-type-120 mr-[.5px]">v</span>
+                <span className="animate-type-130 mr-[.5px]">e</span>
+                <span className="animate-type-140 mr-[.5px]">l</span>
+                <span className="animate-type-150 mr-[.5px]">o</span>
+                <span className="animate-type-160 mr-[.5px]">p</span>
+                <span className="animate-type-170 mr-[.5px]">e</span>
+                <span className="animate-type-180 mr-[12px] lg:mr-[16px]">r</span>
+                <span className="animate-type-0">/</span>
+            </p>
         </header>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { transform } from 'framer-motion';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
@@ -70,8 +72,6 @@ export default {
         "9.1xl": "116px",
         "9xl": "115px",
         "13xl": "144px",
-        // Fix bug with this size in Google Chrome
-        // "13xl": "144px",
       },
       inset: {
         "nav-middle": "calc(50% - 272px)"
@@ -81,6 +81,54 @@ export default {
       },
       boxShadow: {
         "xl-wide": "0 8px 25px 0",
+      },
+      animation: {
+        // Title animation
+        "grow-0": "growUp .5s .3s both",
+        "grow-10": "growUp .5s .36s both",
+        "grow-20": "growUp .5s .42s both",
+        "grow-30": "growUp .5s .48s both",
+        "grow-40": "growUp .5s .54s both",
+        "grow-50": "growUp .5s .60s both",
+        "grow-60": "growUp .5s .66s both",
+        "grow-70": "growUp .5s .72s both",
+        "grow-80": "growUp .5s .78s both",
+        "grow-90": "growUp .5s .84s both",
+        "grow-100": "growUp .5s .90s both",
+        // Subtitle animation
+        "type-0": "type .01s 1.5s both",
+        "type-10": "type .01s 1.54s both",
+        "type-20": "type .01s 1.58s both",
+        "type-30": "type .01s 1.62s both",
+        "type-40": "type .01s 1.66s both",
+        "type-50": "type .01s 1.7s both",
+        "type-60": "type .01s 1.74s both",
+        "type-70": "type .01s 1.78s both",
+        "type-80": "type .01s 1.82s both",
+        "type-90": "type .01s 1.86s both",
+        "type-100": "type .01s 1.9s both",
+        "type-110": "type .01s 1.94s both",
+        "type-120": "type .01s 1.98s both",
+        "type-130": "type .01s 2.02s both",
+        "type-140": "type .01s 2.06s both",
+        "type-150": "type .01s 2.1s both",
+        "type-160": "type .01s 2.14s both",
+        "type-170": "type .01s 2.18s both",
+        "type-180": "type .01s 2.22s both",
+      },
+      keyframes: {
+        growUp: {
+          "0%": { transform: "translateY(40px) scale(0)" },
+          '100%': { transform: "translateY(0) scale(1)" },
+        },
+        type: {
+          "0%": { fontSize: "0" },
+          "100%": { fontSize: "inherit" },
+        },
+        visible: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
       },
     },
   },
